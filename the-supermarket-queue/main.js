e6ba8b0a19bba12f65998050ff368bf5d066e481
Karthik Raju <karthikraju391@gmail.com>
2022-05-23 20:53:42 +0530

    codewars solution #14

---
+++ the-supermarket-queue/main.js
@@ -0,0 +1,13 @@
+function queueTime(customers, registers) {
+   let arr = [];
+ 
+   for(let i = 0; i < registers; i++)
+     arr[i] = 0; //[0, 0]
+ 
+   for(let i = 0; i < customers.length; i++) {
+     arr[0] += customers[i];
+     arr.sort((a, b) => a - b); 
+   }
+ 
+   return arr[arr.length - 1]
+ }
