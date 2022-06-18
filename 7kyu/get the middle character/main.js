a3b3a89c93f945ffd7c27b76aa4a0a103a90d89c
Karthik Raju <karthikraju391@gmail.com>
2022-06-18 23:54:43 +0530

    codewars solution #34

---
+++ 7kyu/get the middle character/main.js
@@ -0,0 +1,8 @@
+function getMiddle(s) {
+   let arr = s.split('')
+   if (arr.length % 2 !== 0) {
+      return arr[Math.floor(arr.length / 2)]
+   } else {
+      return arr[Math.floor(arr.length / 2) - 1] + arr[Math.floor(arr.length / 2)]
+   }
+}
